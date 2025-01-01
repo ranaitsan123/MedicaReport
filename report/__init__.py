14e4ed7cebbd720ae4b0a530cc7f43c5c86822b8
MarwaneMLE <khmarwane10@gmail.com>
2025-01-01 21:35:23 +0100

    setup database

---
+++ report/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "dfgD@sd&sdj!cd"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+    db.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
